9759d796e6feccc30a7bb2beac91a0cedbf733e4
luciana-t <luciana.tavares@ufv.com>
2019-04-28 07:54:44 -0300

    Initial commit

---
+++ app/src/main/java/com/luciana/l3_37/MainActivity.java
@@ -0,0 +1,18 @@
+package com.luciana.l3_37;
+
+import android.support.v4.view.MotionEventCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
