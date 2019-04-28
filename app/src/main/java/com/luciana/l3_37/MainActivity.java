88cfbeffbe529090b039ad76a13bcb2c3e5625dc
luciana-t <luciana.tavares@ufv.com>
2019-04-28 08:20:38 -0300

    Using onTouchEvent Toast with coordinates for entire screen

--- app/src/main/java/com/luciana/l3_37/MainActivity.java
+++ app/src/main/java/com/luciana/l3_37/MainActivity.java
@@ -1,11 +1,11 @@
 package com.luciana.l3_37;
 
-import android.support.v4.view.MotionEventCompat;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.ViewConfiguration;
-import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView im = (ImageView) findViewById(R.id.iml337);
+
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // MotionEvent object holds X-Y values
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            String text = "You click at x = " + event.getX() + " and y = " + event.getY();
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+        }
+
+        return super.onTouchEvent(event);
     }
 
 }
